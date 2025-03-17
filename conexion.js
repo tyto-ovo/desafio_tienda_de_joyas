2b45db7e37c742d1685966f801465ce4fa8c2592
tyto-ovo <sjollyparker@gmail.com>
2025-03-16 21:36:07 -0300

    primeros archivos

---
+++ conexion.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const { Pool } = require("pg");
+const pool = new Pool({
+  host: process.env.PGHOST,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  allowExitOnIdle: true,
+});
+
+module.exports = { pool };
